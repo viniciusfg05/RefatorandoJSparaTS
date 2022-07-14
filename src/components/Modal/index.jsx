51dbaebdde47a86dda47597f7d4535cc32ccf1f2
Vinicius Ferreira <vinicius.fg05@gmail.com>
2022-07-13 21:12:30 -0300

    Componentes concluido

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,28 +1,8 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
-
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+function Modal({isOpen, setIsOpen, children}) {
+  const modalStatus = isOpen
 
     return (
       <ReactModal
@@ -52,7 +32,62 @@ class Modal extends Component {
         {children}
       </ReactModal>
     );
-  }
 };
 
 export default Modal;
+
+// class Modal extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     const { isOpen } = this.props;
+//     this.state = {
+//       modalStatus: isOpen
+//     }
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     const { isOpen } = this.props;
+
+//     if (prevProps.isOpen !== isOpen) {
+//       console.log(this.props)
+//       this.setState({ modalStatus: isOpen })
+//     }
+//   }
+
+//   render() {
+//     const { children, setIsOpen } = this.props;
+//     const { modalStatus } = this.state;
+
+//     return (
+//       <ReactModal
+//         shouldCloseOnOverlayClick={!false}
+//         onRequestClose={setIsOpen}
+//         isOpen={modalStatus}
+//         ariaHideApp={false}
+//         style={{
+//           content: {
+//             top: '50%',
+//             left: '50%',
+//             right: 'auto',
+//             bottom: 'auto',
+//             marginRight: '-50%',
+//             transform: 'translate(-50%, -50%)',
+//             background: '#F0F0F5',
+//             color: '#000000',
+//             borderRadius: '8px',
+//             width: '736px',
+//             border: 'none',
+//           },
+//           overlay: {
+//             backgroundColor: '#121214e6',
+//           },
+//         }}
+//       >
+//         {children}
+//       </ReactModal>
+//     );
+//   }
+// };
+
+// export default Modal;
